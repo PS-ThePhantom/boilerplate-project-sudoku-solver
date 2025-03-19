907f785339da300030ef35ff4359a1725d08d013
Phuluso Singo <Phuluso.singo@gmail.com>
2025-03-19 15:38:17 +0000

    feat: add row and column tests and logic

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,13 +1,43 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    const validChars =/^[1-9.]+$/;
+    
+    return {
+      length: puzzleString.length === 81,
+      chars: validChars.test(puzzleString)
+    };
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
+    const rowIndex = {
+      A: 0,
+      B: 1,
+      C: 2,
+      D: 3,
+      E: 4,
+      F: 5,
+      G: 6,
+      H: 7,
+      I: 8
+    };
+    row = rowIndex[row.toUpperCase()];
 
+    const rows = puzzleString.match(/.{9}/g);
+    const rowToCheck = rows[row];
+    return !rowToCheck.includes(value);
   }
 
   checkColPlacement(puzzleString, row, column, value) {
+    column--;
+
+    const columns = ["", "", "", "", "", "", "", "", ""];
+    for (let i = 0; i < 9; i++) {
+      columns[i] += puzzleString.match(/.{9}/g).map(row => row[i]);
+      columns[i] = columns[i].replace(/,/g, "");
+    }
+    const colToCheck = columns[column];
+    return !colToCheck.includes(value);
 
   }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,4 +6,45 @@ let solver;
 
 suite('Unit Tests', () => {
 
+    test('Logic handles a valid puzzle string of 81 characters', function() {
+        solver = new Solver();
+        const puzzle = '123456789123456789123456789123456789123456789123456789123456789123456789123456789';
+        assert.isTrue(solver.validate(puzzle).chars && solver.validate(puzzle).length);
+    });
+
+    test('Logic handles a puzzle string with invalid characters (not 1-9 or .)', function() {
+        solver = new Solver();
+        const puzzle = '12345678912345678912345678912345678912345678912345678912345678912345678912345678x';
+        assert.isFalse(solver.validate(puzzle).chars);
+    });
+
+    test('Logic handles a puzzle string that is not 81 characters in length', function() {
+        solver = new Solver();
+        const puzzle = '12345678912345678912345678912345678912345678912345678912345678912345678912345678';
+        assert.isFalse(solver.validate(puzzle).length);
+    });
+
+    test('Logic handles a valid row placement', function() {
+        solver = new Solver();
+        const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+        assert.isTrue(solver.checkRowPlacement(puzzle, 'A', 1, 7));
+    });
+
+    test('Logic handles an invalid row placement', function() {
+        solver = new Solver();
+        const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+        assert.isFalse(solver.checkRowPlacement(puzzle, 'A', 1, 9));
+    });
+
+    test('Logic handles a valid column placement', function() {
+        solver = new Solver();
+        const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+        assert.isTrue(solver.checkColPlacement(puzzle, 'A', 1, 7));
+    });
+
+    test('Logic handles an invalid column placement', function() {
+        solver = new Solver();
+        const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+        assert.isFalse(solver.checkColPlacement(puzzle, 'A', 1, 8));
+    });
 });
